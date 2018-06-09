8c80b09adb168096fc0f0f2925a8ce25a2969ee7
Alan David dos Santos <alan_david1992@hotmail.com>
2018-06-09 14:18:51 -0300

    Agregado test

--- UnitTest/UnitTest.cs
+++ UnitTest/UnitTest.cs
@@ -10,7 +10,7 @@ namespace UnitTest
     public class UnitTest
     {
         public char[,] matriz = MatrizHelper.matriz;
-       
+
         [ClassInitialize]
         public static void InitClass(TestContext ctx)
         {
@@ -21,11 +21,16 @@ namespace UnitTest
         {
             IManagerSearch managerSearch = new ManagerSearch();
             var resp1 = managerSearch.findWord(matriz, "JAVA");
-            var resp2 = managerSearch.findWord(matriz, "VIACOM");
-            var resp3 = managerSearch.findWord(matriz, "TELEFE");
             Assert.IsNotNull(resp1);
+            Assert.AreEqual("[2,2,][3,3,][4,4,][5,5,]", resp1);
+            var resp2 = managerSearch.findWord(matriz, "VIACOM");
             Assert.IsNotNull(resp2);
+            Assert.AreEqual("[1,3,][2,3,][3,3,][4,3,][5,3,][6,3,]", resp2);
+            var resp3 = managerSearch.findWord(matriz, "TELEFE");
             Assert.IsNotNull(resp3);
+            Assert.AreEqual("[7,1,][7,2,][7,3,][7,4,][7,5,][7,6,]", resp3);
+
+        
         }
 
 
